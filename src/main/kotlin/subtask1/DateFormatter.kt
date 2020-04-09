7bcdab03eef103993f80e38388c34d475511ebe5
mrnik96 <nekalo96@gmail.com>
2020-04-09 20:10:15 +0300

    done DateFormatter

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -1,9 +1,20 @@
 package subtask1
 
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
+
 class DateFormatter {
 
-    // TODO: Complete the following function
     fun toTextDay(day: String, month: String, year: String): String {
-        throw NotImplementedError("Not implemented")
+        return try {
+            val newMonth = if (month.length == 1) "0$month" else month
+            val language = Locale("ru", "RU")
+            val pattern = "dd MMMM, EEEE"
+            val date = LocalDate.parse("$year-$newMonth-$day")
+            date.format(DateTimeFormatter.ofPattern(pattern, language))
+        } catch (e: Exception){
+            "Такого дня не существует"
+        }
     }
 }
